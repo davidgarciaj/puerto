ee680082d74c94b3af84992e6ee99183d3a198b7
DavidG <igdrasilenllamas@gmail.com>
2016-05-27 14:13:04 +0200

    El puerto puede realizar todas las consultas que deseemos

--- Puerto.java
+++ Puerto.java
@@ -9,12 +9,51 @@ public class Puerto
 {
     // instance variables - replace the example below with your own
     private Alquiler[] alquileres;
+    private int NUMERO_AMARRES = 4;
 
     /**
      * Constructor for objects of class Puerto
      */
     public Puerto()
     {
-        alquileres = new Alquiler[4];
+        alquileres = new Alquiler[NUMERO_AMARRES];
+    }
+
+    /**
+     * 
+     */
+    public int addAlquiler(int dias, Cliente cliente, Barco barco){
+        boolean creado = false;
+        int posicion = -1;
+        int i = 0;
+        while(posicion < NUMERO_AMARRES && !creado){
+            if(alquileres[i] == null){
+                alquileres[i] = new Alquiler(dias, cliente, barco);
+                posicion = i;
+                creado = true;
+            }
+            i++;
+        }
+        return posicion;
+    }
+
+    /**
+     * 
+     */
+    public void verEstadoAmarre(){
+        for(int i = 0; i < NUMERO_AMARRES; i++){
+            if(alquileres[i] == null){
+                System.out.println(alquileres[i]);
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    public float liquidarAlquiler(int posicion){
+        Alquiler alquiler = alquileres[posicion];
+        alquileres[posicion] = null;
+        return alquiler.getCosteAlquiler();
     }
 }
