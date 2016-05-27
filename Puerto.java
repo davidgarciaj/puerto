d0087d81ef72cfa372d677373b74f620f130b9d9
DavidG <igdrasilenllamas@gmail.com>
2016-05-27 13:32:23 +0200

    Preparando el puerto para que use el resto del programa

---
+++ Puerto.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Puerto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Puerto
+{
+    // instance variables - replace the example below with your own
+    private Alquiler[] alquileres;
+
+    /**
+     * Constructor for objects of class Puerto
+     */
+    public Puerto()
+    {
+        alquileres = new Alquiler[4];
+    }
+}
